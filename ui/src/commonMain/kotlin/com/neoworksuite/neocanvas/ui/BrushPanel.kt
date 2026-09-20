76118dc3833b11f027a5e61598364b3de40477e0
neoworkssuite <neoworks@ctec-shop.co.uk>
2026-09-20 19:35:58 +0100

    Fix Brush Studio scrolling import

--- ui/src/commonMain/kotlin/com/neoworksuite/neocanvas/ui/BrushPanel.kt
+++ ui/src/commonMain/kotlin/com/neoworksuite/neocanvas/ui/BrushPanel.kt
@@ -20,6 +20,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Slider
