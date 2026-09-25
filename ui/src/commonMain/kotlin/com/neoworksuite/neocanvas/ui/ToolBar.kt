7840c9ca0765135ed2b07fad650979486d7fbc7a
christianrobertson36 <115557556+christianrobertson36@users.noreply.github.com>
2026-09-25 19:45:32 +0100

    Fix toolbar curve path import

--- ui/src/commonMain/kotlin/com/neoworksuite/neocanvas/ui/ToolBar.kt
+++ ui/src/commonMain/kotlin/com/neoworksuite/neocanvas/ui/ToolBar.kt
@@ -48,6 +48,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.input.pointer.pointerInput
