f001728f78c6c8d89e8ead59fbed64754548d721
neoworkssuite <neoworks@ctec-shop.co.uk>
2026-09-25 21:30:50 +0100

    Register NeoCanvas Windows file association

--- windowsApp/build.gradle.kts
+++ windowsApp/build.gradle.kts
@@ -35,11 +35,18 @@ compose.desktop {
             packageName = "NeoCanvas"
             packageVersion = "1.0.0"
             vendor = "NeoWorksSuite"
+            fileAssociation(
+                mimeType = "application/x-neocanvas",
+                extension = "neocanvas",
+                description = "NeoCanvas Document",
+                windowsIconFile = rootProject.file("assets/branding/neocanvas.ico"),
+            )
             windows {
                 iconFile.set(rootProject.file("assets/branding/neocanvas.ico"))
                 menuGroup = "NeoWorksSuite"
                 shortcut = true
                 menu = true
+                upgradeUuid = "A9E2E7D8-0F2E-4D85-A6A8-7A2FBA9E71C1"
             }
         }
     }
