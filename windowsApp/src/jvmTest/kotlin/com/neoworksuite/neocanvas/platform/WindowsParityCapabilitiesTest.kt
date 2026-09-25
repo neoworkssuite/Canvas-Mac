33b20c216fa10584f0e425e1f5cb70b3f261938e
neoworkssuite <neoworks@ctec-shop.co.uk>
2026-09-25 21:04:05 +0100

    Test Windows parity host capabilities

---
+++ windowsApp/src/jvmTest/kotlin/com/neoworksuite/neocanvas/platform/WindowsParityCapabilitiesTest.kt
@@ -0,0 +1,24 @@
+package com.neoworksuite.neocanvas.platform
+
+import kotlin.test.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class WindowsParityCapabilitiesTest {
+    @Test
+    fun professional_exports_expose_shared_windows_capabilities() {
+        val actions = WindowsEditorFileActions()
+        assertTrue(actions.supportsPsdImport)
+        assertTrue(actions.supportsPsdExport)
+        assertTrue(actions.supportsEditableObjectPsdFlattening)
+        assertTrue(actions.supportsJpegExport)
+        assertTrue(actions.supportsTiffExport)
+        assertFalse(actions.supportsPdfExport)
+    }
+
+    @Test
+    fun external_urls_require_https() {
+        val actions = WindowsEditorFileActions()
+        assertFalse(actions.openExternalUrl("http://example.invalid"))
+    }
+}
