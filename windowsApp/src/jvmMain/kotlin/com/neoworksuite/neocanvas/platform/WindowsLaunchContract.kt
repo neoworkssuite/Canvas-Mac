191129a8c7b580d7b7e721664443079ef49b16ff
neoworkssuite <neoworks@ctec-shop.co.uk>
2026-09-25 19:54:34 +0100

    Align Windows installer launch contract with 1.0

--- windowsApp/src/jvmMain/kotlin/com/neoworksuite/neocanvas/platform/WindowsLaunchContract.kt
+++ windowsApp/src/jvmMain/kotlin/com/neoworksuite/neocanvas/platform/WindowsLaunchContract.kt
@@ -2,5 +2,5 @@ package com.neoworksuite.neocanvas.platform
 
 object WindowsLaunchContract {
     const val runtimeRelativePath = "windowsApp/build/compose/binaries/main/app/NeoCanvas/NeoCanvas.exe"
-    const val installerRelativePath = "windowsApp/build/compose/binaries/main/exe/NeoCanvas-0.1.0.exe"
+    const val installerRelativePath = "windowsApp/build/compose/binaries/main/exe/NeoCanvas-1.0.0.exe"
 }
